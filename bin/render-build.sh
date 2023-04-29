126ddf2812e63bfc0730c3a3294f8038283a856a
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-04-29 16:55:00 -0700

    another

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ bundle install
 
 rm -rf public
 
-yarn /cat-tinder-frontend/package.json  && yarn build /cat-tinder-frontend/package.json
+yarn package.json  && yarn build cat-tinder-frontend
 
 bundle exec rake db:migrate
 

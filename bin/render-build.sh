d8109282d11955bdfa3c365db81e2ce33b473e37
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-04-30 13:57:41 -0700

    updated render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,7 @@
-
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Add build commands for front end
-rm -rf public
-yarn --prefix cat-tinder-frontend && yarn build --prefix cat-tinder-frontend
-cp -a cat-tinder-frontend/build/. public/
-
 bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:seed
 bundle exec rake db:migrate
-bundle exec rails db:seed
